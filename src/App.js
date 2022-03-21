c966ea6c5b78c7a44c1373a811405d939a29e575
daya2940 <sauuit2940@gmail.com>
2022-03-22 00:06:02 +0530

    Added redux store and logout using firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "antd/dist/antd.css";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 import { ToastContainer } from "react-toastify";
-
 import { Routes, Route } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
+import { LOGGED_IN_USER } from "./constants/userConstants";
+
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unSubscribe = auth.onAuthStateChanged(async (user) => {
+      console.log(user);
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: LOGGED_IN_USER,
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    return () => unSubscribe();
+  }, []);
   return (
     <>
       <Header />
--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { LOGOUT } from "../../constants/userConstants";
+import { MailOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import { useDispatch } from "react-redux";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const { SubMenu } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("mail");
+  const dispatch = useDispatch();
+
   const handleClick = (e) => {
     setCurrent(e.key);
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: LOGOUT,
+      payload: null,
+    });
   };
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
@@ -24,6 +36,11 @@ const Header = () => {
       <Menu.Item key="Register" icon={<UserOutlined />}>
         <Link to="/register">Register</Link>
       </Menu.Item>
+      <SubMenu key="Username" icon={<SettingOutlined />} title="UserName">
+        <Menu.Item key="logout" onClick={handleLogout}>
+          Logout
+        </Menu.Item>
+      </SubMenu>
     </Menu>
   );
 };
